be742619b387c460b0455ae56441d83494964f57
Delon Ansah Acquaah <delonansah87@gmail.com>
2025-06-13 14:39:52 +0000

    added lazy loading

--- client/src/components/pages/Projects.jsx
+++ client/src/components/pages/Projects.jsx
@@ -47,6 +47,7 @@ const Projects = () => {
               <img
                 src={mainImage.asset.url}
                 alt={title}
+                 loading="lazy"
                 className={`w-full h-full object-cover group-hover:scale-110 transition-transform duration-500 ${isListView ? 'h-48 sm:h-full' : 'h-64'}`}
               />
               <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
